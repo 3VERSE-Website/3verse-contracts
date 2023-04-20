ca0c461a0518c23e0d73705c72de2e213db24cdb
seigneur <>
2023-04-20 08:00:14 +0800

    Added script for deployment

--- script/Token.s.sol
+++ script/Token.s.sol
@@ -1,12 +1,25 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.17;
 
-import "forge-std/Script.sol";
+import {CREATE3Script} from "./base/CREATE3Script.sol";
+import {Token} from "src/token/Token.sol";
 
-contract CounterScript is Script {
-    function setUp() public {}
+contract DeployScript is CREATE3Script {
+    constructor() CREATE3Script(vm.envString("VERSION")) {}
 
-    function run() public {
-        vm.broadcast();
+    function run() external returns (Token c) {
+        uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
+
+        address param = msg.sender;
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        c = Token(
+            create3.deploy(
+                getCreate3ContractSalt("Token"), bytes.concat(type(Token).creationCode, abi.encode(param))
+            )
+        );
+
+        vm.stopBroadcast();
     }
 }

3a319081de20ae1f95a0fd662bef61b2e7bfbaad
Corrino <92191590+shaddamciv@users.noreply.github.com>
2023-06-07 23:24:05 +0800

    Update Token.sol with antisnipe

--- src/token/Token.sol
+++ src/token/Token.sol
@@ -1,10 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity 0.8.20;
 
 import "openzeppelin/token/ERC20/ERC20.sol";
 import "openzeppelin/token/ERC20/extensions/ERC20Burnable.sol";
 import "openzeppelin/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "openzeppelin/access/AccessControl.sol";
+
+interface IAntisnipe {
+    function assureCanTransfer(
+        address sender,
+        address from,
+        address to,
+        uint256 amount
+    ) external;
+}
 
 /// @title 3Verse Token contract
 contract Token is ERC20, ERC20Burnable, ERC20Permit, AccessControl {
@@ -22,6 +31,9 @@ contract Token is ERC20, ERC20Burnable, ERC20Permit, AccessControl {
     address public constant PARTNERS_ADVISORS_ADDRESS = address(0x125);
     address public constant MARKETING_ADDRESS = address(0x126);
     
+    IAntisnipe public antisnipe;
+    bool public antisnipeDisable;
+    
     constructor(address owner) ERC20("3VERSE", "3VE") ERC20Permit("3VERSE") {
         _grantRole(DEFAULT_ADMIN_ROLE, owner);
         _grantRole(MINTER_ROLE, owner);
@@ -38,5 +50,24 @@ contract Token is ERC20, ERC20Burnable, ERC20Permit, AccessControl {
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         require(totalSupply() + amount <= MAX_SUPPLY, "Max supply reached");
         _mint(to, amount);
+    }
+    
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {
+        if (from == address(0) || to == address(0)) return;
+        if (!antisnipeDisable && address(antisnipe) != address(0))
+            antisnipe.assureCanTransfer(msg.sender, from, to, amount);
+    }
+
+    function setAntisnipeDisable() external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(!antisnipeDisable);
+        antisnipeDisable = true;
+    }
+
+    function setAntisnipeAddress(address addr) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        antisnipe = IAntisnipe(addr);
     }
 }

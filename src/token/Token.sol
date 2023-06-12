cab5127922a30429f9a34c7b264220f49ab70525
corrino <corrino>
2023-06-12 21:40:00 +0800

    Keep version same as audited copy

--- src/token/Token.sol
+++ src/token/Token.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "openzeppelin/token/ERC20/ERC20.sol";
-import "openzeppelin/token/ERC20/extensions/ERC20Burnable.sol";
-import "openzeppelin/token/ERC20/extensions/draft-ERC20Permit.sol";
-import "openzeppelin/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 interface IAntisnipe {
     function assureCanTransfer(
